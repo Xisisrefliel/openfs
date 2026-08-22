79f5674b3d6c119fab86afe1abb30aa6de413eb3
omerguel <feherlofia@icloud.com>
2026-08-22 20:11:30 +0200

    Memoize chart and toggle-group context values

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -32,6 +32,11 @@ function ToggleGroup({
     spacing?: number;
     orientation?: "horizontal" | "vertical";
   }) {
+  const contextValue = React.useMemo(
+    () => ({ variant, size, spacing, orientation }),
+    [variant, size, spacing, orientation],
+  );
+
   return (
     <ToggleGroupPrimitive.Root
       data-slot="toggle-group"
@@ -46,7 +51,7 @@ function ToggleGroup({
       )}
       {...props}
     >
-      <ToggleGroupContext.Provider value={{ variant, size, spacing, orientation }}>
+      <ToggleGroupContext.Provider value={contextValue}>
         {children}
       </ToggleGroupContext.Provider>
     </ToggleGroupPrimitive.Root>
